e73dee6d2a5f0037118b0b8835cd2b67f5e44639
erickz5501 <54655666+erickz5501@users.noreply.github.com>
2022-02-01 22:57:13 -0500

    Template

---
+++ resources/views/layouts/theme/scripts.blade.php
@@ -0,0 +1,18 @@
+<!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
+<script src=" {{ asset('assets/js/libs/jquery-3.1.1.min.js')}} "></script>
+<script src=" {{ asset('bootstrap/js/popper.min.js') }} "></script>
+<script src=" {{ asset('bootstrap/js/bootstrap.min.js') }} "></script>
+<script src=" {{ asset('plugins/perfect-scrollbar/perfect-scrollbar.min.js') }} "></script>
+<script src=" {{ asset('assets/js/app.js')}} "></script>
+<script>
+    $(document).ready(function() {
+        App.init();
+    });
+</script>
+<script src=" {{ asset('assets/js/custom.js')}} "></script>
+<!-- END GLOBAL MANDATORY SCRIPTS -->
+
+<!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
+<script src=" {{ asset('plugins/apex/apexcharts.min.js') }} "></script>
+<script src=" {{ asset('assets/js/dashboard/dash_1.js')}} "></script>
+<!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
---
+++ resources/views/layouts/theme/styles.blade.php
@@ -0,0 +1,13 @@
+<link href=" {{ asset('assets/css/loader.css') }}" rel="stylesheet" type="text/css" />
+<script src=" {{ asset('assets/js/loader.js') }}"></script>
+
+<!-- BEGIN GLOBAL MANDATORY STYLES -->
+<link href="https://fonts.googleapis.com/css?family=Quicksand:400,500,600,700&display=swap" rel="stylesheet">
+<link href=" {{ asset('bootstrap/css/bootstrap.min.css') }} " rel="stylesheet" type="text/css" />
+<link href=" {{ asset('assets/css/plugins.css') }}" rel="stylesheet" type="text/css" />
+<link href=" {{ asset('assets/css/structure.css') }}" rel="stylesheet" type="text/css" class="structure" />
+<!-- END GLOBAL MANDATORY STYLES -->
+
+<!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM STYLES -->
+<link href=" {{ asset('plugins/apex/apexcharts.css') }} " rel="stylesheet" type="text/css">
+<link href=" {{ asset('assets/css/dashboard/dash_1.css') }}" rel="stylesheet" type="text/css" class="dashboard-analytics" />
