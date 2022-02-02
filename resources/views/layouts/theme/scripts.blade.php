bac122af6c741d2461c5aaca3748f3fa6ae43b08
erickz5501 <54655666+erickz5501@users.noreply.github.com>
2022-02-02 00:04:47 -0500

    Plugins

--- resources/views/layouts/theme/scripts.blade.php
+++ resources/views/layouts/theme/scripts.blade.php
@@ -10,9 +10,27 @@
     });
 </script>
 <script src=" {{ asset('assets/js/custom.js')}} "></script>
+<script src=" {{ asset('plugins/sweetalerts/sweetalert2.min.js') }} "></script>
+<script src=" {{ asset('plugins/notification/snackbar/snackbar.min.js') }} "></script>
+<script src=" {{ asset('plugins/nicescroll/nicescroll.js') }} "></script>
+<script src=" {{ asset('plugins/currency/currency.js') }} "></script>
+
 <!-- END GLOBAL MANDATORY SCRIPTS -->
 
 <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
 <script src=" {{ asset('plugins/apex/apexcharts.min.js') }} "></script>
 <script src=" {{ asset('assets/js/dashboard/dash_1.js')}} "></script>
 <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
+
+<script>
+    function noty(msg, option = 1){
+        Snackbar.show({
+            text: msg.toUpperCase(),
+            actionText: 'Cerrar',
+            actionTextColor: '#fff',
+            backgroundColor: option == 1 ? '#3b3f5c' : '#e7515a',
+            pos: 'top-right'
+        });
+    }
+</script>
+@livewireScripts
--- resources/views/layouts/theme/styles.blade.php
+++ resources/views/layouts/theme/styles.blade.php
@@ -8,6 +8,45 @@
 <link href=" {{ asset('assets/css/structure.css') }}" rel="stylesheet" type="text/css" class="structure" />
 <!-- END GLOBAL MANDATORY STYLES -->
 
+<link rel="stylesheet" href=" {{ asset('plugins/font-icons/fontawesome/css/fontawesome.css') }}" type="text/css" />
+<link rel="stylesheet" href=" {{ asset('css/fontawesome.css') }}" type="text/css" />
+<link rel="stylesheet" href=" {{ asset('assets/css/elements/avatar.css') }}" type="text/css" />
+<link rel="stylesheet" href=" {{ asset('plugins/sweetalerts/sweetalert.css') }} ">
+<link rel="stylesheet" href=" {{ asset('plugins/notification/snackbar/snackbar.min.css') }} " type="text/css" />
+
+<link rel="stylesheet" href=" {{ asset('css/custom.css') }}" type="text/css" />
+
+<link rel="stylesheet" href=" {{ asset('assets/css/widgets/modules-widgets.css') }}" type="text/css" />
+<link rel="stylesheet" href=" {{ asset('assets/css/forms/theme-checkbox-radio.css') }}" type="text/css" />
+
 <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM STYLES -->
 <link href=" {{ asset('plugins/apex/apexcharts.css') }} " rel="stylesheet" type="text/css">
 <link href=" {{ asset('assets/css/dashboard/dash_1.css') }}" rel="stylesheet" type="text/css" class="dashboard-analytics" />
+
+<style>
+    aside{
+        display: none!important;
+    }
+    .page-item.active .page-link {
+        z-index: 3;
+        color: #fff;
+        background-color: #3b3f5c;
+        border-color: #3b3f5c;
+    }
+
+    @media(max-width:480px){
+        .mtmobile {
+            margin-bottom: 20px!important;
+        }
+        .mbmobile{
+            margin-bottom: 10px!important;
+        }
+        .hideinsm{
+            display: none!important;
+        }
+        .inblock{
+            display: block;
+        }
+    }
+</style>
+@livewireStyles
