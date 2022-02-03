f6ed45deae9066248bc81d993db7431e1ff8c24d
PieroLopez12 <pedrolopez@upeu.edu.pe>
2022-02-02 19:37:26 -0500

    seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,9 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(CategorySeeder::class);
+        $this->call(DenominationSeeder::class);
+        $this->call(ProductSeeder::class);
+        $this->call(Type_clientSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
