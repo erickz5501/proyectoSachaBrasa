235ff62c6805cdc9ba7dd101658317abc1bd5c8c
erickz5501 <54655666+erickz5501@users.noreply.github.com>
2022-02-07 17:40:33 -0500

    Avanze ventas

--- resources/views/livewire/pos/component.blade.php
+++ resources/views/livewire/pos/component.blade.php
@@ -1,21 +1,52 @@
-<div>
-    <style></style>
+<div class="row sales layout-top-spacing">
+    <div class="col-sm-12">
+        <div class="widget widget-chart-one">
+            <div class="widget-heading">
+                <h4 class="cart-title">
+                    <b> {{$componentName}} | {{$pagetitle}}</b>
+                </h4>
+                <ul class="tabs tab-pills">
+                    <li>
+                        <button href="javascript:void(0);" class="btn btn-dark px-3 py-2" data-toggle="modal" data-target="#theModal" type="button">
+                            <i class="fas fa-plus-circle"></i>
+                            <span>Agregar</span>
+                        </button>
+                    </li>
+                </ul>
+            </div>
 
-    <div class="row layout-top-spacing">
-        <div class="col-sm-12 col-md-8">
-            {{-- Detalles --}}
-            @include('livewire.pos.partials.detail')
-        </div>
-        <div class="col-sm-12 col-md-4">
-            {{-- Total --}}
-            @include('livewire.pos.partials.total')
-            {{-- Denominaciones --}}
-            @include('livewire.pos.partials.coins')
+            <div class="widget-content">
+
+                <div class="table-responsive">
+                    <table class="table table-bordered table-striped mt-1">
+                        <thead class="text-white" style=" background: #3B3F5C ">
+                            <tr>
+                                <th class="table-th text-white">Descripción</th>
+                                <th class="table-th text-white">Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td><h6>Nombre</h6></td>
+                                <td class="text-center">
+                                    <a href="javascript:void(0)" class="btn btn-dark mtmobile" title="Editar">
+                                        <i class="fas fa-edit"></i>
+                                    </a>
+                                    <a href="javascript:void(0)" class="btn btn-dark mtmobile" title="Eliminar">
+                                        <i class="fas fa-trash"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
-
 </div>
 
 <script>
-    
-</script>
+    document.addEventListener('DOMContentLoaded', function(){
+
+    });
+  </script>
