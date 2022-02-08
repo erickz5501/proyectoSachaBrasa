12b02ba88806b8cdaeab739f39e9a7d436fdba69
AlexanderRomeroo <alexanderromero@upeu.edu.pe>
2022-02-08 16:46:57 -0500

    avance de configuración

--- resources/views/livewire/settings/settings.blade.php
+++ resources/views/livewire/settings/settings.blade.php
@@ -1,51 +1,140 @@
 <div class="row sales layout-top-spacing">
-    <div class="col-sm-12">
-        <div class="widget widget-chart-one">
-            <div class="card">
-                <div class="row g-0">
-                    <div class="col-1">
-                        <img src="{{ asset('assets/images/theme/settings.jpg')}}"  weight="100px" height="100px" alt="...">
-                    </div>
-                <div class="col-md-8">
-                <div class="card-body">
-                    <h3 class="card-title "> Central de Ajustes</h3>
-                    <p class="card-text"> Controla todos los ajustes del sistema desde este modulo</p>
-                </div>
-                </div>
+   <div class="col-sm-12">
+      <div class="widget widget-chart-one">
+         <div class="card">
+            <div class="row g-0">
+               <div class="col-1">
+                  <img src="{{ asset('assets/images/theme/settings.jpg')}}"  weight="100px" height="100px" alt="...">
+               </div>
+               <div class="col-md-8">
+                  <div class="card-body">
+                     <h3 class="card-title "> Central de Ajustes</h3>
+                     <p class="card-text"> Controla todos los ajustes del sistema desde este modulo</p>
+                  </div>
+               </div>
             </div>
-            </div>
-            <br>
-            <div class="row row-cols-1 row-cols-md-3 g-4">
+         </div>
+         <br>
+         <div class="row row-cols-4 row-cols-md-3 g-4">
             <div class="card" style="width: 18rem;">
-                <div class="card-header">
-                     Featured
-                </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">An item</li>
-                <li class="list-group-item">A second item</li>
-                <li class="list-group-item">A third item</li>
-            </ul>
+               <div class="card-header">
+                  <b>Empresa</b>
+               </div>
+               <div>
+                  <ul class="list-group">
+                     <a href="">
+                        <b>
+                           <li class="list-group-item">
+                              Datos de la empresa
+                        </b>
+                        <p>Modificar datos de la empresa</p>
+                        </li>
+                     </a>
+                  </ul>
+               </div>
+               <div>
+                  <ul class="list-group">
+                     <a href="">
+                        <b>
+                           <li class="list-group-item">
+                              Tipo de Documentos
+                        </b>
+                        <p>Modificar los tipos de documentos</p>
+                        </li>
+                     </a>
+                  </ul>
+               </div>
+               <div>
+                  <ul class="list-group">
+                     <a href="">
+                        <b>
+                           <li class="list-group-item">
+                              Usuarios / Roles
+                        </b>
+                        <p>Creación, Modificación.</p>
+                        </li>
+                     </a>
+                  </ul>
+               </div>
             </div>
-            </div> 
-            <div class="row row-cols-1 row-cols-md-3 g-4">
+         </div>
+
+
+         <div class="row row-cols-4 row-cols-md-3 g-4">
             <div class="card" style="width: 18rem;">
-                <div class="card-header">
-                     Featured
-                </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">An item</li>
-                <li class="list-group-item">A second item</li>
-                <li class="list-group-item">A third item</li>
-            </ul>
+               <div class="card-header">
+                  <b>Restaurante</b>
+               </div>
+               <div>
+                  <ul class="list-group">
+                     <a href="">
+                        <b>
+                           <li class="list-group-item">
+                              Cajas
+                        </b>
+                        <p>Creación, Modificación.</p>
+                        </li>
+                     </a>
+                  </ul>
+               </div>
+               <div>
+                  <ul class="list-group">
+                     <a href="">
+                        <b>
+                           <li class="list-group-item">
+                              Áreas de producción
+                        </b>
+                        <p>Creación, Modificación.</p>
+                        </li>
+                     </a>
+                  </ul>
+               </div>
+               <div>
+                  <ul class="list-group">
+                     <a href="">
+                        <b>
+                           <li class="list-group-item">
+                              Salones y mesas
+                        </b>
+                        <p>Creación, Modificación.</p>
+                        </li>
+                     </a>
+                  </ul>
+               </div>
+               <div>
+                  <ul class="list-group">
+                     <a href="">
+                        <b>
+                           <li class="list-group-item">
+                              Productos
+                        </b>
+                        <p>Creación, Modificación.</p>
+                        </li>
+                     </a>
+                  </ul>
+               </div>
             </div>
-            </div> 
-        </div>  
-    </div>    
-</div>      
+         </div>
 
-
-
-
-
-
-
+         <div class="row row-cols-4 row-cols-md-3 g-4">
+            <div class="card" style="width: 18rem;">
+               <div class="card-header">
+                  <b>Otros Ajustes</b>
+               </div>
+               <div>
+                  <ul class="list-group">
+                     <a href="">
+                        <b>
+                           <li class="list-group-item">
+                              Impresoras
+                        </b>
+                        <p>Creación, Modificación.</p>
+                        </li>
+                     </a>
+                  </ul>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
