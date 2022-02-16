dcb10cdd28666c63dab8760669a4357249c1267c
erickz5501 <54655666+erickz5501@users.noreply.github.com>
2022-02-15 22:36:29 -0500

    Presentación

--- resources/views/livewire/pos/component.blade.php
+++ resources/views/livewire/pos/component.blade.php
@@ -26,64 +26,6 @@
                         </div>
                     </div>      
                 </div> 
-                <br><br>
-                <div class="row">
-                    <div class="col-sm-12">
-                        <div class="">
-                            <div class=""">
-                                <h4 class="cart-title">
-                                    <b> Productos </b>
-                                </h4>
-                            </div>
-                
-                            <div class="widget-content">
-                
-                                <ul class="nav nav-tabs">
-                                    @foreach($categories as $category)
-                                    <li class="nav-item">
-                                        <a data-toggle="tab" href="#cat{{$category->id}}" type="button" class="nav-link">{{$category->name}}</a>
-                                    </li>
-                                    @endforeach
-                                </ul>
-                                
-                                <div class="tab-content">
-                                    @foreach($categories as $category)
-                                    <div id="cat{{$category->id}}" class="tab-pane fade">
-                                        <div class="table-responsive">
-                                            <table class="table table-bordered table-striped mt-1" style="text-align:center;">
-                                                <thead class="text-white" style=" background: #e05f1a ">
-                                                    <tr >
-                                                        <th class="table-th text-white text-center">Nombre</th>
-                                                        <th class="table-th text-white text-center">Descripción</th>
-                                                        <th class="table-th text-white text-center">Precio</th>
-                                                        <th class="table-th text-white text-center">Stock</th>
-                                                        <th class="table-th text-white text-center">Inv. Min</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                        @foreach($products as $product)
-                                                            @if($product->category->id == $category->id)
-                                                                <tr>
-                                                                    <td><h6> {{$product->name}}</h6></td>
-                                                                    <td ><h6 class="text-center"> {{$product->description}}</h6></td>
-                                                                    <td><h6 class="text-center">S/. {{$product->price}}</h6></td>
-                                                                    <td><h6 class="text-center"> {{$product->stock}} </h6></td>
-                                                                    <td><h6 class="text-center"> {{$product->alerts}} </h6></td>
-                                                                </tr>
-                                                             @endif
-                                                        @endforeach
-                                                </tbody>
-                                            </table>
-                                            {{-- {{$products->links()}} --}}
-                                        </div>
-                                    </div>
-                                @endforeach
-                                </div>
-            
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
             
         </div>
